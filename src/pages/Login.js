a1344687f400aedaeccb5877afdf88e5b8bf1ada
parkduhwan <enghksqkr05@gmail.com>
2024-07-16 02:20:59 +0900

    빌드오류 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import api from '../services/api';
 import toast from 'react-simple-toasts';
 import styles from '../assets/styles/login.module.css';
+import Loading from '../components/common/Loading';
 
 const Login = ({ setIsLoggedIn }) => {
   const navigate = useNavigate();
@@ -28,48 +29,53 @@ const Login = ({ setIsLoggedIn }) => {
     }
   };
 
+  if (loading) {
+    return <div><Loading /></div>;
+  }
+
   return (
     <div className={styles['login-container']}>
       <main className={styles['form-signin']}>
-        <form onSubmit={handleSubmit}>
-          <img className="mb-4" src="../assets/images/features-first-icon.png" alt="" width="72" height="57" />
-          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-          <div className="form-floating">
-            <input
-              type="email"
-              className="form-control"
-              id="floatingInput"
-              placeholder="name@example.com"
-              value={id}
-              onChange={(e) => setId(e.target.value)}
-            />
-            <label htmlFor="floatingInput">Email address</label>
-          </div>
-          <div className="form-floating">
-            <input
-              type="password"
-              className="form-control"
-              id="floatingPassword"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <label htmlFor="floatingPassword">Password</label>
-          </div>
-          <div className="form-check text-start my-3">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="remember-me"
-              id="flexCheckDefault"
-            />
-            <label className="form-check-label" htmlFor="flexCheckDefault">
-              Remember me
-            </label>
-          </div>
-          <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
-          <p className="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
-        </form>
+          <form onSubmit={handleSubmit}>
+            <img className="mb-4" src="../assets/images/features-first-icon.png" alt="" width="72" height="57" />
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+            <div className="form-floating">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+              />
+              <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <div className="form-check text-start my-3">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="remember-me"
+                id="flexCheckDefault"
+              />
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Remember me
+              </label>
+            </div>
+            <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+            <p className="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+          </form>
+        )}
       </main>
     </div>
   );
--- src/pages/Today.js
+++ src/pages/Today.js
@@ -1,49 +1,49 @@
 // src/pages/Today.js
-import React, { useEffect, useState } from 'react';
-import Loading from '../components/common/Loading';
-import api from '../services/api';
+// import React, { useEffect, useState } from 'react';
+// import Loading from '../components/common/Loading';
+// import api from '../services/api';
 // import { Card } from '../components/common/Card';
 import styles from '../assets/styles/today.module.scss';
 
 const Today = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [selectedPost, setSelectedPost] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  // const [posts, setPosts] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
+  // const [selectedPost, setSelectedPost] = useState(null);
+  // const [isModalOpen, setIsModalOpen] = useState(false);
 
-  useEffect(() => {
-    const getToday = async () => {
-      try {
-        const response = await api.get('/products');
-        setPosts(response.data);
-        console.log(response.data);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
-      }
-    };
-    getToday();
-  }, []);
+  // useEffect(() => {
+  //   const getToday = async () => {
+  //     try {
+  //       const response = await api.get('/products');
+  //       setPosts(response.data);
+  //       console.log(response.data);
+  //       setLoading(false);
+  //     } catch (error) {
+  //       setError(error);
+  //       setLoading(false);
+  //     }
+  //   };
+  //   getToday();
+  // }, []);
 
-  const openModal = (post) => {
-    setSelectedPost(post);
-    setIsModalOpen(true);
-  };
+  // const openModal = (post) => {
+  //   setSelectedPost(post);
+  //   setIsModalOpen(true);
+  // };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedPost(null);
-  };
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  //   setSelectedPost(null);
+  // };
 
-  if (loading) {
-    return <div><Loading /></div>;
-  }
+  // if (loading) {
+  //   return <div><Loading /></div>;
+  // }
 
-  if (error) {
-    return <p>에러메세지: {error.message}</p>;
-  }
+  // if (error) {
+  //   return <p>에러메세지: {error.message}</p>;
+  // }
 
   return (
     <div className={styles.todayWrapper}>
