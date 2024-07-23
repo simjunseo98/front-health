5496607b9e7a0186e6fd8319affe241e86bbc0ba
parkduhwan <enghksqkr05@gmail.com>
2024-07-23 20:30:23 +0900

    로그인 화면 수정

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import api from '../services/api';
 import axios from 'axios';
 import toast from 'react-simple-toasts';
 import styles from '../assets/styles/login.module.css';
 import Loading from '../components/common/Loading';
+import Logo from '../assets/images/dumbel.jpg';
 
 const Login = ({ setIsLoggedIn }) => {
   const navigate = useNavigate();
@@ -16,7 +16,6 @@ const Login = ({ setIsLoggedIn }) => {
     e.preventDefault();
     setLoading(true);
     try {
-      // const response = await api.post('/user/login', { id, password });
       const response = await axios.post('https://trendy-healthy-backend.store/jwt/authenticate', { id, password });
       console.log('Login successful:', response.data);
       const token = response.data.token;
@@ -39,18 +38,18 @@ const Login = ({ setIsLoggedIn }) => {
     <div className={styles['login-container']}>
       <main className={styles['form-signin']}>
         <form onSubmit={handleSubmit}>
-          <img className="mb-4" src="../assets/images/features-first-icon.png" alt="" width="72" height="57" />
-          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+          <img src={Logo} alt="Logo" />
+          <h1 className="h3 mb-3 fw-normal">Login</h1>
           <div className="form-floating">
             <input
-              type="email"
+              type="text"
               className="form-control"
               id="floatingInput"
               placeholder="name@example.com"
               value={id}
               onChange={(e) => setId(e.target.value)}
             />
-            <label htmlFor="floatingInput">Email address</label>
+            <label htmlFor="floatingInput">아이디</label>
           </div>
           <div className="form-floating">
             <input
@@ -61,7 +60,7 @@ const Login = ({ setIsLoggedIn }) => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <label htmlFor="floatingPassword">Password</label>
+            <label htmlFor="floatingPassword">패스워드</label>
           </div>
           <div className="form-check text-start my-3">
             <input
@@ -74,7 +73,7 @@ const Login = ({ setIsLoggedIn }) => {
               Remember me
             </label>
           </div>
-          <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+          <button className="btn btn-primary w-100 py-2" type="submit">로그인</button>
           <p className="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
         </form>
       </main>
