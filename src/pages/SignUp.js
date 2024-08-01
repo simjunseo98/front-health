6f5545554bbb44c6250c42d8dcd3d2ec076e374c
parkduhwan <enghksqkr05@gmail.com>
2024-08-01 18:37:37 +0900

    로그인 빌드 오류 해결

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import toast from 'react-simple-toasts';
 import { useNavigate } from 'react-router-dom';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
