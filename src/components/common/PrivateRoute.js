21d49b6e0bc258458ed968db88a12e5bfa584e9a
parkduhwan <enghksqkr05@gmail.com>
2024-08-12 16:02:37 +0900

    로컬스토리지 -> 세션스토리지로 변경

--- src/components/common/PrivateRoute.js
+++ src/components/common/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 //로그인되지 않은 사용자를 막기위한 컴포넌트
 const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
+  const token = sessionStorage.getItem('token');
 
   if (!token) {
     alert('로그인이 필요합니다.');
